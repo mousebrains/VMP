d9a5e96ff11220021d7b5a0fa8af472cea07ac23
Pat Welch <mousebrains@users.noreply.github.com>
2023-07-30 18:48:18 -0700

    Add depth calculation

--- addSeawaterProperties.m
+++ addSeawaterProperties.m
@@ -11,4 +11,5 @@ slow.JAC_SA = gsw_SA_from_SP(slow.JAC_SP, slow.P_slow, profile.lon, profile.lat)
 slow.JAC_theta = gsw_CT_from_t(slow.JAC_SA, slow.JAC_T, slow.P_slow); % Conservation T
 slow.JAC_sigma = gsw_sigma0(slow.JAC_SA, slow.JAC_theta);
 slow.JAC_rho = gsw_rho(slow.JAC_SA, slow.JAC_theta, slow.P_slow) - 1000; % density kg/m^3 - 1000
+slow.depth = gsw_depth_from_z(gsw_z_from_p(slow.P_slow, profile.lat));
 end % addSeawaterProperties
